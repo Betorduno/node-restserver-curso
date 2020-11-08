2c3d6117408bf1bd9cb6421f82c86f19cb0fd682
Betorduno <alberto.rodriguez.davila@gmail.com>
2020-11-08 10:00:56 -0500

    restserver - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,18 @@
 //puerto
  
 process.env.PORT = process.env.PORT || 3000
+
+//entorno
+
+process.env.NODE_ENV = process.env.NODE_ENV || 3000
+
+//base de datos
+let urlDB ;
+
+if(process.env.NODE_ENV === 'dev') {
+ urlDB ='mongodb://localhost:27017/cafe'
+} else {
+  urlDB ='mongodb+srv://strider:xy174alb@cluster0.bpchn.mongodb.net/test'
+}
+
+process.env.URLDB = urlDB;
---
+++ server/routes/usuarios.js
@@ -0,0 +1,124 @@
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+const _ = require('underscore')
+const bodyParser = require('body-parser')
+const Usuario = require('../models/usuario')
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/usuario', function (req, res) {
+
+  let desde =req.query.desde || 0
+  desde = Number(desde);
+
+  let limite = req.query.limite || 5
+  limite = Number(limite);
+  //todos los registro de la colección 
+  Usuario.find({ estado: true }, 'nombre email img role estado google') //con el string propuesto se visualizan solo los campos que se encuetran en el string
+  //salta de $desde
+  .skip(desde)
+  //solo 5 registros
+  .limit(5)
+    .exec((err, usuarios) => {
+      if(err) {
+        return res.status(400).json({
+          ok:false,
+          err
+        })
+      } 
+      //contar la cantidad de registro de la base de datos 
+      Usuario.count({ estado: true }, (err, conteo)=>{
+        res.json({
+          ok: true,
+          usuarios,
+          registros: conteo
+        })
+      })
+
+      
+    })
+})
+
+app.post('/usuario', function (req, res) {
+  let body = req.body
+  
+  let user = new Usuario({
+    nombre: body.nombre,
+    email: body.email,
+    password: bcrypt.hashSync(body.password, 10) ,
+    role: body.role
+  })
+
+  user.save((err, usuarioDB)=> {
+    if(err) {
+      return res.status(400).json({
+        ok:false,
+        err
+      })
+    } 
+    res.json({
+      ok: true,
+      usuario:usuarioDB
+    })
+  })
+})
+
+app.put('/usuario/:id', function (req, res) {
+
+  let id = req.params.id;
+  //(underscore)_.pick regresa un object filtrado por los campos que se estipulan en el array.
+
+  let body = _.pick(req.body,['nombre', 'email','img','role','estado'] )
+  
+  Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB)=> {
+    if(err) {
+      return res.status(400).json({
+        ok:false,
+        err
+      })
+    } 
+    res.json({
+      ok: true,
+      usuario:usuarioDB
+    })
+  })
+})
+
+app.delete('/usuario/:id', function (req, res) {
+
+  let id = req.params.id;
+  let cambiaEstado = {
+    estado: false
+  }
+  // Usuario.findByIdAndRemove(id, (err, userDelete) => {
+  Usuario.findByIdAndUpdate(id, cambiaEstado, { new:true }, (err, userDelete) => {
+    if(err) {
+      return res.status(400).json({
+        ok:false,
+        err
+      })
+    } 
+    if(!userDelete) {
+      return res.status(400).json({
+        ok:false,
+        err: {
+          message: 'Usuario no encontrado'
+        }
+      })
+    }
+    res.json({
+      ok: true,
+      usuario: userDelete
+    })
+
+  })
+
+})
+
+module.exports = app;
